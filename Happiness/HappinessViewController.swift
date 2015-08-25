7e9c2ce83f35b0b0812bf213e8bd8586702c0a51
dimitar <dimityr.danailov@gmail.com>
2015-08-25 19:10:37 +0300

    Add first implementation of Happiness.

---
+++ Happiness/HappinessViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by dimitar on 8/25/15.
+//  Copyright (c) 2015 dimityr.danailov. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController
+{
+    
+}
